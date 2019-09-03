f9f4dc7376fa6c17520d31616a48deb14ab460fc
Karan Kochar <karankochar30@gmail.com>
2019-09-04 02:01:43 +0530

    Navbar added

---
+++ src/Components/Navbar.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import './navbar.css';
+
+export default class Navbar extends Component{
+    render(){
+        return(
+            <div className='navbar'>
+                <header>
+                    <h3>MicroURL</h3>
+                <nav>
+                <ul class='links'>
+                    <li><a href= '#'>Home</a></li>
+                    <li><a href= '#'>About</a></li>
+                    <li><a href= '#'>Pricing</a></li>
+                    <li><a href= '#'>Solutions</a></li>
+                </ul>
+                </nav>
+                <a class='cta' href='#'><button>Login</button></a>
+                </header>
+
+            </div>
+        );
+    }
+
+}
